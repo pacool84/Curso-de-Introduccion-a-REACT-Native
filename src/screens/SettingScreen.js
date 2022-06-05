80c3dccbcfa05ad9583f3e938ef65ac6ad003a9d
Francisco López <flopez_1384@hotmail.com>
2022-06-05 16:29:25 -0500

    [ADD] Logic to navigate with buttons between Home and Settings screens

--- src/screens/SettingScreen.js
+++ src/screens/SettingScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 
-const SettingScreen = () => {
+const SettingScreen = (props) => {
+  const { navigation } = props;
+
+  const goToPage = (pageName) => {
+    navigation.navigate(pageName);
+  };
   return (
     <View>
       <Text>Hello my friend SETTING SCREEN ⚙️</Text>
+      <Button title="Ir a Home" onPress={() => goToPage("Home")} />
     </View>
   );
 };
